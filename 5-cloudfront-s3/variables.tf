be2b08d71ce73cda95e6d71166d52b9ed3700438
Nicky Santiago <santiagonicky@icloud.com>
2026-06-01 05:14:06 +0000

    Testing Jenkins build pipeline with a minor insignificant change

--- 5-cloudfront-s3/variables.tf
+++ 5-cloudfront-s3/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  description = "AWS region for this environment"
+  description = "The AWS region for this environment"
   type        = string
   default     = "us-east-1"
 }
